d0f50b66ebc979b7f6ca1cc30b50aa808091ca22
s-shchekoldin <shchekoldin@gmail.com>
2024-08-06 21:12:53 +0100

    improve perfomance, less branches

--- xml.cc
+++ xml.cc
@@ -1,6 +1,6 @@
 // ==============================================================
-// Date: 2024-07-24 20:19:32 GMT
-// Generated using vProto(2024.07.24)         https://www.vsyn.ru
+// Date: 2024-08-06 20:12:46 GMT
+// Generated using vProto(2024.08.06)         https://www.vsyn.ru
 // Author: Sergey V. Shchekoldin     Email: shchekoldin@gmail.com
 // ==============================================================
 
@@ -70,7 +70,7 @@ inline void xml::parse(state_t & state)
             case node_t::NO_STATE:
             default: return;
         }; // switch
-        if (n == state.node && d == state.data)
+        if (d == state.data && n == state.node) [[unlikely]]
             break;
     }
 }
--- xml.h
+++ xml.h
@@ -1,6 +1,6 @@
 // ==============================================================
-// Date: 2024-07-24 20:19:32 GMT
-// Generated using vProto(2024.07.24)         https://www.vsyn.ru
+// Date: 2024-08-06 20:12:46 GMT
+// Generated using vProto(2024.08.06)         https://www.vsyn.ru
 // Author: Sergey V. Shchekoldin     Email: shchekoldin@gmail.com
 // ==============================================================
 
